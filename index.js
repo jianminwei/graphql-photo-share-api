f71f9aa4fdde1e36ea365e03d59dced690e001f1
Jianmin <jianminwei1@gmail.com>
2019-05-09 08:17:36 -0400

    added inPhoto at User and taggedUsers at Photo, and their resolvers. Beautiful code

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ type Photo {
     description: String
     category: PhotoCategory!
     postedBy: User!
+    taggedUsers: [User!]!
+    
 }
 
 type User {
@@ -24,6 +26,7 @@ type User {
     name: String
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
 }
 
 # 2. Return Photo from allPhotos
@@ -75,6 +78,13 @@ let photos = [
     }
 ]
 
+var tags = [
+    { "photoID": "1", "userID": "gPlake" },
+    { "photoID": "2", "userID": "sSchmidt" },
+    { "photoID": "2", "userID": "mHattrup" },
+    { "photoID": "2", "userID": "gPlake" }
+]
+
 const resolvers = {
     Query: {
         totalPhotos: () => photos.length,
@@ -95,12 +105,27 @@ const resolvers = {
         url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
         postedBy: parent => {
             return users.find(u => u.githubLogin === parent.githubUser)
-        }
+        },
+        taggedUsers: parent => tags
+            // Returns an array of tags that only contain the current photo
+            .filter(tag => tag.photoID === parent.id)
+            // Converts the array of tags into an array of userIDs
+            .map(tag => tag.userID)
+            // Converts array of userIDs into an array of user objects
+            .map(userID => users.find(u => u.githubLogin === userID))
     },
+
     User: {
         postedPhotos: parent => {
             return photos.filter(p => p.githubUser === parent.githubLogin)
-        }
+        },
+        inPhotos: parent => tags
+            // Returns an array of tags that only contain the current user
+            .filter(tag => tag.userID === parent.id)
+            // Converts the array of tags into an array of photoIDs
+            .map(tag => tag.photoID)
+            // Converts array of photoIDs into an array of photo objects
+            .map(photoID => photos.find(p => p.id === photoID))
     }
 }
 
