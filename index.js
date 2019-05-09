6d23dbc025a86226465f18e9fbbc68a0b5cfe5c4
Jianmin <jianminwei1@gmail.com>
2019-05-09 14:20:08 -0400

    added DateTime custom scalar type, and used it in Photo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { ApolloServer } = require('apollo-server')
+const { GraphQLScalarType } = require('graphql')
 
 const typeDefs = `
+scalar DateTime
+
 enum PhotoCategory {
     SELFIE
     PORTRAIT
@@ -18,7 +21,7 @@ type Photo {
     category: PhotoCategory!
     postedBy: User!
     taggedUsers: [User!]!
-    
+    created: DateTime!
 }
 
 type User {
@@ -62,20 +65,23 @@ let photos = [
         "name": "Dropping the Heart Chute",
         "description": "The heart chute is one of my favorite chutes",
         "category": "ACTION",
-        "githubUser": "gPlake"
+        "githubUser": "gPlake",
+        "created": "3-28-1977"
     },
     {
         "id": "2",
         "name": "Enjoying the sunshine",
         "category": "SELFIE",
-        "githubUser": "sSchmidt"
+        "githubUser": "sSchmidt",
+        "created": "1-2-1985"
     },
     {
         id: "3",
         "name": "Gunbarrel 25",
         "description": "25 laps on gunbarrel today",
         "category": "LANDSCAPE",
-        "githubUser": "sSchmidt"
+        "githubUser": "sSchmidt",
+        "created": "2018-04-15T19:09:57.308Z"
     }
 ]
 
@@ -97,7 +103,8 @@ const resolvers = {
         postPhoto(parent, args) {
             var newPhoto = {
                 id: _id++,
-                ...args.input
+                ...args.input,
+                created: new Date()
             }
             photos.push(newPhoto)
             return newPhoto
@@ -128,7 +135,15 @@ const resolvers = {
             .map(tag => tag.photoID)
             // Converts array of photoIDs into an array of photo objects
             .map(photoID => photos.find(p => p.id === photoID))
-    }
+    },
+
+    DateTime: new GraphQLScalarType({
+        name: 'DateTime',
+        description: 'A valid date time value.',
+        parseValue: value => new Date(value),
+        serialize: value => new Date(value).toLocaleString(),
+        parseLiteral: ast => ast.value
+    })
 }
 
 // 2. Create a new instance of the server.
