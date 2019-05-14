b60d615118d1c7f1f9724b93a5c8af2a17f0c0cb
Jianmin <jianminwei1@gmail.com>
2019-05-14 06:45:32 -0400

    refactored code, moved resolvers to its own code

--- index.js
+++ index.js
@@ -3,109 +3,10 @@ const express = require('express')
 const expressPlayground = require('graphql-playground-middleware-express').default
 const { readFileSync } = require('fs')
 const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
-//const {resolvers} = require('./resolvers/index.js')
-const { GraphQLScalarType } = require('graphql')
+const resolvers = require('./resolvers')
 
 const { MongoClient } = require('mongodb')
 require('dotenv').config()
-
-const resolvers = {
-    Query: {
-        totalPhotos: (parent, args, { db }) =>
-            db.collection('photos')
-                .estimatedDocumentCount(),
-
-        allPhotos: (parent, args, { db }) =>
-            db.collection('photos')
-                .find()
-                .toArray(),
-
-        totalUsers: (parent, args, { db }) =>
-            db.collection('users')
-                .estimatedDocumentCount(),
-                
-        allUsers: (parent, args, { db }) =>
-            db.collection('users')
-                .find()
-                .toArray()
-    },
-
-    Mutation: {
-        postPhoto(parent, args, { db, getSequence }) {
-
-            /****************************************************
-             * Post a new photo syntax:
-             * 
-                mutation newPhoto($input: PostPhotoInput!) {
-                postPhoto(input: $input) {
-                    name
-                    url
-                    description
-                    category
-                    postedBy{
-                        githubLogin
-                        name
-                    }
-                    taggedUsers {
-                        name
-                    }
-                  }
-                }
-
-                *** Query Variable ***
-                {
-                    "input": {
-                        "name": "React workshop",
-                        "description": "Photo taked at react workshop",
-                        "githubUser": "sSchmidt"
-                    }
-                }                
-
-             ****************************************************/
-
-            var newPhoto = {
-                ...args.input,
-                taggedUsers:[],
-                created: new Date()
-            }
-
-            db.collection("photos").insertOne(newPhoto)
-            return newPhoto
-        }
-    },
-
-    Photo: {
-        url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
-        postedBy: (parent, args, { db }) => 
-            db.collection('users')
-            .findOne({githubLogin: parent.githubUser}),
-        
-        taggedUsers: (parent, args, { db }) =>
-            db.collection('users')
-            .find({githubLogin: {"$in": parent.taggedUsers} })
-            .toArray()
-    },
-
-    User: {
-        postedPhotos: (parent, args, { db }) =>
-            db.collection('photos')
-            .find({ githubUser: parent.githubLogin } )
-            .toArray() ,
-
-        inPhotos: (parent, args, { db }) =>
-            db.collection('photos')
-            .find({ taggedUsers: parent.githubLogin })
-            .toArray()
-    },
-
-    DateTime: new GraphQLScalarType({
-        name: 'DateTime',
-        description: 'A valid date time value.',
-        parseValue: value => new Date(value),
-        serialize: value => new Date(value).toLocaleString(),
-        parseLiteral: ast => ast.value
-    })
-}
 
 // 1. Create Asynchronous Function
 async function start() {
@@ -118,7 +19,6 @@ async function start() {
     )
 
     const db = client.db()
-
     const context = { db }
     const server = new ApolloServer({ typeDefs, resolvers, context })
 
--- resolvers/index.js
+++ resolvers/index.js
@@ -1,6 +1,101 @@
-
 const { GraphQLScalarType } = require('graphql')
 
+const resolvers = {
+    Query: {
+        totalPhotos: (parent, args, { db }) =>
+            db.collection('photos')
+                .estimatedDocumentCount(),
 
+        allPhotos: (parent, args, { db }) =>
+            db.collection('photos')
+                .find()
+                .toArray(),
 
+        totalUsers: (parent, args, { db }) =>
+            db.collection('users')
+                .estimatedDocumentCount(),
+                
+        allUsers: (parent, args, { db }) =>
+            db.collection('users')
+                .find()
+                .toArray()
+    },
 
+    Mutation: {
+        postPhoto(parent, args, { db, getSequence }) {
+
+            /****************************************************
+             * Post a new photo syntax:
+             * 
+                mutation newPhoto($input: PostPhotoInput!) {
+                postPhoto(input: $input) {
+                    name
+                    url
+                    description
+                    category
+                    postedBy{
+                        githubLogin
+                        name
+                    }
+                    taggedUsers {
+                        name
+                    }
+                  }
+                }
+
+                *** Query Variable ***
+                {
+                    "input": {
+                        "name": "React workshop",
+                        "description": "Photo taked at react workshop",
+                        "githubUser": "sSchmidt"
+                    }
+                }                
+
+             ****************************************************/
+
+            var newPhoto = {
+                ...args.input,
+                taggedUsers:[],
+                created: new Date()
+            }
+
+            db.collection("photos").insertOne(newPhoto)
+            return newPhoto
+        }
+    },
+
+    Photo: {
+        url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+        postedBy: (parent, args, { db }) => 
+            db.collection('users')
+            .findOne({githubLogin: parent.githubUser}),
+        
+        taggedUsers: (parent, args, { db }) =>
+            db.collection('users')
+            .find({githubLogin: {"$in": parent.taggedUsers} })
+            .toArray()
+    },
+
+    User: {
+        postedPhotos: (parent, args, { db }) =>
+            db.collection('photos')
+            .find({ githubUser: parent.githubLogin } )
+            .toArray() ,
+
+        inPhotos: (parent, args, { db }) =>
+            db.collection('photos')
+            .find({ taggedUsers: parent.githubLogin })
+            .toArray()
+    },
+
+    DateTime: new GraphQLScalarType({
+        name: 'DateTime',
+        description: 'A valid date time value.',
+        parseValue: value => new Date(value),
+        serialize: value => new Date(value).toLocaleString(),
+        parseLiteral: ast => ast.value
+    })
+}
+
+module.exports = resolvers
