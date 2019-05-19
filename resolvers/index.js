c3556fe73d4e9839b7d1f362320f589d907f70be
Jianmin <jianminwei1@gmail.com>
2019-05-19 08:24:26 -0400

    added fakeUserAuth mutation query

--- resolvers/index.js
+++ resolvers/index.js
@@ -138,6 +138,17 @@ const resolvers = {
             }))
             await db.collection('users').insert(users)
             return users
+        },
+        
+        async fakeUserAuth(parent, { githubLogin }, { db }) {
+            var user = await db.collection('users').findOne({ githubLogin })
+            if (!user) {
+                throw new Error(`Cannot find user with githubLogin "${githubLogin}"`)
+            }
+            return {
+                token: user.githubToken,
+                user
+            }
         }          
     },
 
